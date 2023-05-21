13304e8b5bebb39d80200e19c0ecf1bbcfa6a7a6
noaman jebreel <m.jebreel22@gmail.com>
2023-05-21 15:08:36 +0300

    first commit

---
+++ resources/views/rows.blade.php
@@ -0,0 +1,44 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Users') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Row Number</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Registered at</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(count($users)>0)
+                            @foreach ($users as $user)
+                                {{-- Task: only every second row should have "bg-red-100" --}}
+                                <tr class="{{$loop->iteration % 2 == 0 ? 'bg-red-100' : ''}}">
+                                    <td>{{$loop->iteration}}</td>
+                                    <td>{{ $user->name }}</td>
+                                    <td class="{{$loop->iteration == 1 ? 'font-bold' : ''}}">{{ $user->email }}</td>
+                                    <td>{{ $user->created_at }}</td>
+                                </tr>
+                            @endforeach
+                            @else
+                            <tr>
+                                <td colspan="3">No content.</td>
+                            </tr>
+
+                            @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
